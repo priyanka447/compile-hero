c9dcb86090cbb2347f36f24075a5221eb619300d
priyanka447 <90445422+priyanka447@users.noreply.github.com>
2021-09-14 15:36:24 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,105 @@
+import.react.from.react-redux;
+import.statement.from.redux;
+import.script.from.screen;
+
+<div.className="script">
+<Navbar.filter="screen">{floor.Screen.media}</Navbar>
+<Filter.array="area">{media.redux.floor}</Filter>
+<Statement.area="app">{statement.random.floor}</Statement>
+<Stop.app="array">{floor.script.screen}</Stop>
+<Storage.name="navbar">{media.floor.random.media}</Strong>
+<Array.class="name">{floor.random.media}</Array>
+<Style.array="react">{media.screen.floor}</Style>
+<Class.statement="floor">{floor.area.array}</Class>
+<Svg.media="random">{floor.screen.svg}</Svg>
+<Strict.app="filter">{area.media.screen}</Strict>
+<Symbol.filter="media">{media.screen.floor}</Symboi>
+</div>
+
+
+
+
+App.css
+*
+{
+....margin:.0px;
+....padding:.0px;
+....box-sizing:.boader-box;
+....justify-content:.center;
+....align-items:.center;
+....display:.block;
+....text-align:center;
+....float:.left;
+....font-style:normal;
+....text-align:center;
+....float:.left;
+....font-style:normal;
+....text-justify:.inherit;
+....box-sizing:.boader-box;
+....font-size:.15px;
+....font-weight:.500;
+....font-family:.'montserrat',.sans-serif;
+....border:.1px.sodid.#fff;
+....-color:.#fff;
+....color:#000;
+....boarder-radius:.10px;
+....boader-left:.100px;
+....boader-right:.100px;
+....boader-top:.100px;
+....boader-bottom:.100px;
+....boader-style:.none;
+....boader-width:.100px;
+....boader-height:.100px;
+....width:.100px;
+....height:.100px;
+....boader-none;
+....outline:none;
+....list-style:none;
+....list-style-type:.none;
+....flex-direction:.column;
+....flex-wrap:wrap-reverse;
+....flex-flow:.coloumn;
+....flex-basic:.content;
+....flex-glow:.inherit;
+....flex-shrink:.inherit;
+....direction:.inherit;
+}
+
+
+style.html
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tutorial</title>
+        <link rel="stylesheet"herf="style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <h1>front page</h1>
+            <img Class="image-weather" src="img/weather.app"alt=""
+        </div>
+        </body>
+   </html>  
+
+
+
+style.image
+*{
+    margin:0;
+    padding:0;
+}
+
+.wrapper {
+    margin:0 auto;
+    width:1024px
+}
+
+.img-weather app {
+    width:500px;
+    height:300px;
+    background-image;url(img/weather.aap);
+    background-size:cover;
+}
+
